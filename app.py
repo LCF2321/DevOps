cfedabc93aafc56d7aa191b0793d809eaddc695c
LCF2321 <c0919344@mylambton.ca>
2024-10-18 01:56:38 -0400

    Initial

---
+++ app.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Access the environment variables
+MONGODB_USERNAME = os.getenv('negiseema621')
+MONGODB_PASSWORD = os.getenv('Seema%4024')
+
+
+
+
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Replace with your MongoDB connection string
+client = MongoClient("mongodb+srv://negiseema621:Seema%4024@shop-db.j4ylx.mongodb.net/?retryWrites=true&w=majority&appName=shop-db")
+db = client.shop_db
+products_collection = db.products
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/products')
+def products():
+    products_data = list(products_collection.find())
+    return render_template('products.html', products=products_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
